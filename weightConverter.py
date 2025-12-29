d576ff1f22ef8754e193e52ec8f5fdb831386539
Sayan Mallick <malsayan6@gmail.com>
2025-12-29 10:31:02 +0530

    fixed

--- weightConverter.py
+++ weightConverter.py
@@ -4,10 +4,11 @@ unit = input("Enter choice: K (for kilogram) L (for pounds): ")
 if unit=="K" :
  weight=weight*2.205
  unit="lbs"
+ print(f"your weight is {round(weight,2)} {unit}")
 elif unit=="L" :
  unit="kg"
  weight=weight/2.205 
+ print(f"your weight is {round(weight,2)} {unit}")
 else :
  print(f"{unit} is not valid")
 
-print(f"your weight is {round(weight,2)} {unit}")
