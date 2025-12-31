eb4fe2519f9c28fd7a4a1bfb3ae966304e8bd259
Sayan Mallick <malsayan6@gmail.com>
2025-12-31 11:26:54 +0530

    pokemon

--- pandas/pokemon.py
+++ pandas/pokemon.py
@@ -2,11 +2,16 @@ import pandas as pd
 
 df =pd.read_csv("pokemon.csv",index_col="name")
 
-pokemon= input("Enter a pokemon name: ")
+# pokemon= input("Enter a pokemon name: ")
 
-try:
-    print(df.loc[pokemon])
-except KeyError:
-    print(f"{pokemon} not found")
+# try:
+#     print(df.loc[pokemon])
+# except KeyError:
+#     print(f"{pokemon} not found")
 
-     
+fast_pokemon= df[df["speed"] >=80 ] 
+attacking_pokemon=df[df["attack"] >=100 ] 
+water_pokemon=df[df["type_1"]=="Water"]
+#print(attacking_pokemon)
+#print (fast_pokemon)
+print(water_pokemon)
