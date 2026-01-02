d453deb65e313c5892bc8403cffd8db6221931a1
Sayan Mallick <malsayan6@gmail.com>
2026-01-02 13:49:54 +0530

    updated

--- pandas/pokemon.py
+++ pandas/pokemon.py
@@ -11,7 +11,7 @@ df =pd.read_csv("pokemon.csv",index_col="name")
 
 fast_pokemon= df[df["speed"] >=80 ] 
 attacking_pokemon=df[df["attack"] >=100 ] 
-water_pokemon=df[(df["type_1"]=="fire") & (df["type_2"]=="flying") ] 
+water_pokemon=df[(df["type_1"]=="Fire") & (df["type_2"]=="Flying") ] 
 #print(attacking_pokemon)
 #print (fast_pokemon)
 print(water_pokemon)
