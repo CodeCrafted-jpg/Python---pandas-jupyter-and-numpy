b62ca97fb087d3e1e792bd99338182c2df4ce6ff
Sayan Mallick <malsayan6@gmail.com>
2026-01-07 21:46:59 +0530

    new

--- numpy/main.py
+++ numpy/main.py
@@ -9,10 +9,13 @@ import numpy as np
 # number=array[0,0,3] + array[0,1,2]
 # print(number)
 #array[start:end:step]
-array=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+array=np.array([[1,2,3,4],
+                [5,6,7,8],
+                [9,10,11,12],
+                [13,14,15,16]])
 
-#print(array[0:3])
+# #print(array[0:3])
 
-#print (array[::-1])
-print(array[:,::2])
+# #print (array[::-1])
+# print(array[:,::2])
 
